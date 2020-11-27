371d950175822e1f766d6a7f0272abc0daed8126
Fabian <fabian.loebel@fau.de>
2020-11-27 23:44:09 +0100

    Added support for playlists by adding loop around existing functionality
    Instead of breaking on first found entry on youtube, each entry is added to a list
    This list is then used to create playable sources in another list which is then returned

--- ytdl.py
+++ ytdl.py
@@ -61,6 +61,9 @@ class YTDLSource(discord.PCMVolumeTransformer):
 
     @classmethod
     async def create_source(cls, ctx: commands.Context, search: str, *, loop: asyncio.BaseEventLoop = None):
+        urls_to_play = []
+        cls_to_play = []
+
         loop = loop or asyncio.get_event_loop()
 
         partial = functools.partial(cls.ytdl.extract_info, search, download=False, process=False)
@@ -72,37 +75,38 @@ class YTDLSource(discord.PCMVolumeTransformer):
         if 'entries' not in data:
             # Case for normal Youtube videos
             process_info = data
-            webpage_url = process_info['webpage_url']
+            urls_to_play.append(process_info['webpage_url'])
         else:
             # Case for Youtube playlists
             process_info = None
             for entry in data['entries']:
                 if entry:
                     process_info = entry
+                    print(process_info)
                     # Returned JSON has no key 'webpage_url', but rather the Video ID accessible at key 'url'
-                    webpage_url = "https://www.youtube.com/watch?v="+process_info['url']
-                    break
+                    urls_to_play.append("https://www.youtube.com/watch?v="+process_info['url'])
 
             if process_info is None:
                 raise YTDLError('Couldn\'t find anything that matches `{}`'.format(search))
 
-        partial = functools.partial(cls.ytdl.extract_info, webpage_url, download=False)
-        processed_info = await loop.run_in_executor(None, partial)
+        for webpage_url in urls_to_play:
+            partial = functools.partial(cls.ytdl.extract_info, webpage_url, download=False)
+            processed_info = await loop.run_in_executor(None, partial)
 
-        if processed_info is None:
-            raise YTDLError('Couldn\'t fetch `{}`'.format(webpage_url))
+            if processed_info is None:
+                raise YTDLError('Couldn\'t fetch `{}`'.format(webpage_url))
 
-        if 'entries' not in processed_info:
-            info = processed_info
-        else:
-            info = None
-            while info is None:
-                try:
-                    info = processed_info['entries'].pop(0)
-                except IndexError:
-                    raise YTDLError('Couldn\'t retrieve any matches for `{}`'.format(webpage_url))
-        
-        return cls(ctx, discord.FFmpegPCMAudio(info['url'], **cls.FFMPEG_OPTIONS), data=info)
+            if 'entries' not in processed_info:
+                info = processed_info
+            else:
+                info = None
+                while info is None:
+                    try:
+                        info = processed_info['entries'].pop(0)
+                    except IndexError:
+                        raise YTDLError('Couldn\'t retrieve any matches for `{}`'.format(webpage_url))
+            cls_to_play.append(cls(ctx, discord.FFmpegPCMAudio(info['url'], **cls.FFMPEG_OPTIONS), data=info))
+        return cls_to_play
 
     @classmethod
     async def search_source(cls, bot: commands.Bot, ctx: commands.Context, search: str, *, loop: asyncio.BaseEventLoop = None):
