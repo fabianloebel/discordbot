c1bcab60cc429342faa2d3820fd2f9313b170d40
Fabian Löbel <fabian@loebelnet.de>
2021-08-03 23:12:52 +0200

    Working server status message

--- cogs/gamesystem.py
+++ cogs/gamesystem.py
@@ -6,16 +6,20 @@ class GameSystem(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.command(name='state', help='Gets server state.')
-    async def _get_service_state(self, ctx: commands.Context, *, service: str):
-        service = service.decode("utf8", "ignore")
+    @commands.command(name='servers', help='Gets server state.')
+    async def _get_service_state(self, ctx: commands.Context):
+        #service = service.decode("utf8", "ignore")
 
         async with ctx.typing():
-            if service in ["vhserver", "sauerbraten-server", "cod4server", "dungeoncrawler"]:
+            states = ""
+            for service in ["vhserver", "sauerbraten-server", "cod4server", "dungeoncrawler"]:
                 unit = Unit(bytes(f'{service}.service', encoding='utf-8'))
                 unit.load()
+                
+                status = unit.Unit.ActiveState.decode("utf8")
+                states += f'\n[{service}] {status}'
 
-                await ctx.send(f'[{service}] {unit.Unit.ActiveState}')
+            await ctx.send(states)
 
 
 
