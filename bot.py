47b5faefa61d26c54d0f90e0f10c26e82d2d713b
Fabian Löbel <fabian@loebelnet.de>
2021-12-23 02:07:18 +0100

    Switched from manual cog loading to discord python specific loader

--- bot.py
+++ bot.py
@@ -1,8 +1,17 @@
 # bot.py
 
-from cogs.music import *
-from cogs.greeter import *
-from cogs.gamesystem import *
+import os
+from dotenv import load_dotenv
+import logging
+
+import discord
+from discord.ext import commands
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 load_dotenv()
 _TOKEN = os.getenv('DISCORD_TOKEN')
@@ -10,9 +19,11 @@ _GUILD = os.getenv('DISCORD_GUILD')
 
 bot = commands.Bot(command_prefix='?')
 
-bot.add_cog(Greeter(bot))
-bot.add_cog(Music(bot))
-bot.add_cog(GameSystem(bot))
+bot.load_extension("cogs.greeter")
+bot.load_extension("cogs.music")
+bot.load_extension("cogs.gamesystem")
+
+
 
 @bot.event 
 async def on_error(event, *args, **kwargs): 
